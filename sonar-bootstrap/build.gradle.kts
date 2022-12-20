83d8c85c69cd4ab38ec3c06b1add998c37a5f4b0
Sabin Popescu <96418974+sabintheodorpopescu@users.noreply.github.com>
2022-12-20 16:06:31 +0200

    Update build.gradle.kts

--- sonar-bootstrap/build.gradle.kts
+++ sonar-bootstrap/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
