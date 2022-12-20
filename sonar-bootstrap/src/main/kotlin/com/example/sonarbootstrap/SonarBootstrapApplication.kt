04f1e306cb367ac097e32484a629e3bf3127ad9a
Sabin Popescu <sabintheodor.popescu@stefanini.com>
2022-12-20 15:24:44 +0200

    Bootstrap project

---
+++ sonar-bootstrap/src/main/kotlin/com/example/sonarbootstrap/SonarBootstrapApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sonarbootstrap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SonarBootstrapApplication
+
+fun main(args: Array<String>) {
+	runApplication<SonarBootstrapApplication>(*args)
+}
